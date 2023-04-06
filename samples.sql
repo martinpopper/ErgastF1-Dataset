521c994070379575cb3a50db9419496226e43ae4
martinpopper <80554637+martinpopper@users.noreply.github.com>
2023-04-05 20:41:20 -0500

    Update samples.sql

--- samples.sql
+++ samples.sql
@@ -39,3 +39,12 @@ FROM Constructor_Standing
 INNER JOIN Constructor ON Constructor_Standing.constructorId = Constructor.constructorId
 WHERE Constructor_Standing.raceId = [raceId]
 ORDER BY Constructor_Standing.points DESC;
+
+--Retrieve the circuit name, race name, and the average lap time for each race held in the circuit with the circuit_id = 5, only for races held in the year 2022 or later, ordered by the average lap time in ascending order:
+SELECT c.name AS circuit_name, r.name AS race_name, AVG(milliseconds) AS average_lap_time
+FROM Circuit c
+INNER JOIN Races r ON c.circuitId = r.circuitId
+INNER JOIN Results rs ON r.raceId = rs.raceId
+WHERE c.circuitId = 5 AND YEAR(r.date) >= 2022
+GROUP BY r.raceId
+ORDER BY average_lap_time ASC;
