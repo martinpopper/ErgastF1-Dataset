8510cd498502900029d63041754d1066f61fefa3
Owen Ostermann <129796691+oostermann10@users.noreply.github.com>
2023-04-05 21:20:10 -0500

    Update samples.sql

--- samples.sql
+++ samples.sql
@@ -1,3 +1,4 @@
+
 -- Select the number of wins for each driver and order them by the number of wins:
 SELECT Drivers.surname, Drivers.forename, COUNT(Driver_Standings.wins) as wins
 FROM Drivers
@@ -22,29 +23,30 @@ GROUP BY Circuit.country
 ORDER BY num_races DESC;
 
 -- Get the number of wins and total points for each driver
-SELECT Drivers.forename, Drivers.surname, COUNT(Driver_Standings.wins) AS wins, SUM(Driver_Standings.points) AS total_points
+SELECT Drivers.driverId, Drivers.forename, Drivers.surname, COUNT(Driver_Standings.wins) AS wins, SUM(Driver_Standings.points) AS total_points
 FROM Drivers
 INNER JOIN Driver_Standings ON Drivers.driverId = Driver_Standings.driverId
-GROUP BY Drivers.driverId;
+GROUP BY Drivers.driverId, Drivers.forename, Drivers.surname;
 
 --Get the average duration of pitstops for each race
-SELECT Races.name, AVG(Pitstop.duration) AS avg_duration
+SELECT Races.name, AVG(Pitstop.milliseconds) AS avg_milliseconds_pitstop
 FROM Races
-LEFT OUTER JOIN Pitstop ON Races.raceId = Pitstop.raceId
-GROUP BY Races.raceId;
+INNER JOIN Pitstop ON Races.raceId = Pitstop.raceId
+GROUP BY Races.name;
 
---Get the constructor standings for a particular race:
+--Get the constructor standings for raceId 100
 SELECT Constructor.name, Constructor_Standing.points, Constructor_Standing.wins
 FROM Constructor_Standing
 INNER JOIN Constructor ON Constructor_Standing.constructorId = Constructor.constructorId
-WHERE Constructor_Standing.raceId = [raceId]
+WHERE Constructor_Standing.raceId = 100
 ORDER BY Constructor_Standing.points DESC;
 
---Retrieve the circuit name, race name, and the average lap time for each race held in the circuit with the circuit_id = 5, only for races held in the year 2022 or later, ordered by the average lap time in ascending order:
-SELECT c.name AS circuit_name, r.name AS race_name, AVG(milliseconds) AS average_lap_time
-FROM Circuit c
-INNER JOIN Races r ON c.circuitId = r.circuitId
-INNER JOIN Results rs ON r.raceId = rs.raceId
-WHERE c.circuitId = 5 AND YEAR(r.date) >= 2022
-GROUP BY r.raceId
-ORDER BY average_lap_time ASC;
+-- retreive the fastets lap speed of the montreal grand prix of the 2022 race to and the drivers name to see which 
+-- drivers had the fastest lap during that race
+SELECT forename, surname, fastestLapSpeed 
+FROM Races
+INNER JOIN Circuit ON Circuit.circuitId = Races.circuitId
+INNER JOIN Results ON Races.raceId = Results.raceId
+INNER JOIN Drivers on Drivers.driverId = Results.driverId
+WHERE Circuit.circuitId = 7 AND YEAR(Races.date) = 2022
+ORDER BY fastestLapSpeed DESC;
